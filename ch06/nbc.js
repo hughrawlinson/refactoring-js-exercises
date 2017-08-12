cece8c38b52f72ba0995c23c4b0db6233696c2d0
Hugh Rawlinson <hugh@spotify.com>
2017-08-12 18:30:43 +0100

    encapsulate song declarations in a function

--- ch06/nbc.js
+++ ch06/nbc.js
@@ -4,15 +4,17 @@ const easy = 'easy';
 const medium = 'medium';
 const hard = 'hard';
 
-imagine = ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'];
-somewhere_over_the_rainbow = ['c', 'em', 'f', 'g', 'am'];
-tooManyCooks = ['c', 'g', 'f'];
-iWillFollowYouIntoTheDark = ['f', 'dm', 'bb', 'c', 'a', 'bbm'];
-babyOneMoreTime = ['cm', 'g', 'bb', 'eb', 'fm', 'ab'];
-creep = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'];
-paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
-toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'];
-bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
+function setSongs() {
+  imagine = ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'];
+  somewhere_over_the_rainbow = ['c', 'em', 'f', 'g', 'am'];
+  tooManyCooks = ['c', 'g', 'f'];
+  iWillFollowYouIntoTheDark = ['f', 'dm', 'bb', 'c', 'a', 'bbm'];
+  babyOneMoreTime = ['cm', 'g', 'bb', 'eb', 'fm', 'ab'];
+  creep = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'];
+  paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
+  toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'];
+  bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
+};
 
 var songs = [];
 var allChords = new Set();
@@ -64,6 +66,7 @@ function setProbabilityOfChordsInLabels(){
 }
 
 function trainAll() {
+  setSongs();
   train(imagine, easy);
   train(somewhere_over_the_rainbow, easy);
   train(tooManyCooks, easy);
